54216d80bb3c8677c51d3c92fea29aab5f4fc747
Francisco Javier Quero Lombardero <franxi2953@gmail.com>
2024-02-27 17:18:30 +0000

    update

--- MiniProject/wordcount.bat
+++ MiniProject/wordcount.bat
@@ -2,5 +2,5 @@
 texcount %1 -out=wordcount.log
 
 for /f "tokens=4 delims=: " %%i in ('findstr /c:"Words in text" wordcount.log') do (
-    echo Word Count: %%i > %~n1.wc
+    echo Main body word count: %%i > %~n1.wc
 )
